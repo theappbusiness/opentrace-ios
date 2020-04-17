0c08147cd2154f2866e6b4b64af3946681d7d986
Neil Horton <neil.horton@santander.co.uk>
2020-04-17 11:58:26 +0100

    create screens for in app messaging

---
+++ OpenTrace/Localization/InAppMessagesStrings.swift
@@ -0,0 +1,21 @@
+//
+//  InAppMessagesStrings.swift
+//  OpenTrace
+//
+//  Created by Neil Horton on 17/04/2020.
+//  Copyright © 2020 OpenTrace. All rights reserved.
+//
+
+import Foundation
+
+extension DisplayStrings {
+
+    enum InAppMessages {
+        
+        enum InfectedContactNotification {
+            static let title = "UploadData.Info.title".localized
+            static let subHeading = "UploadData.Info.subHeading".localized
+            static let primaryCTA = "UploadData.Info.primary_cta".localized
+        }
+    }
+}
